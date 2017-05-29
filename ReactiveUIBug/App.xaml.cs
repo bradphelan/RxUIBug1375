dfcd9dbe5abdfc6a4f915e9d7680d8292bda128b
bradphelan <b.phelan@weingartner.com>
2017-05-29 12:28:02 +0200

    first commit

---
+++ ReactiveUIBug/App.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using System.Windows;
+using ReactiveUI;
+using Splat;
+
+namespace ReactiveUIBug
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup( e );
+
+            Init();
+
+
+
+            var window = new MainWindow();
+            window.Show();
+        }
+
+        public static void Init()
+        {
+
+
+            Locator.CurrentMutable.InitializeReactiveUI();
+        }
+
+
+    }
+
+
+
+    public static class ObservableExtensions
+    {
+
+        public static void LoadUnloadHandler(this FrameworkElement control, Func<IEnumerable<IDisposable>> action)
+        {
+            LoadUnloadHandler(control, () => (IDisposable)new CompositeDisposable(action()));
+        }
+
+        public static void LoadUnloadHandler(this FrameworkElement control, Func<IDisposable> action)
+        {
+            var state = false;
+            var cleanup = new SerialDisposable();
+            Observable.Merge
+                (Observable.Return(control.IsLoaded)
+                , control.Events().Loaded.Select(x => true)
+                , control.Events().Unloaded.Select(x => false)
+                )
+                .Subscribe(isLoadEvent =>
+                {
+                    if (!state)
+                    {
+                        // unloaded state
+                        if (isLoadEvent)
+                        {
+                            state = true;
+                            cleanup.Disposable = new CompositeDisposable(action());
+                        }
+                    }
+                    else
+                    {
+                        // loaded state
+                        if (!isLoadEvent)
+                        {
+                            state = false;
+                            cleanup.Disposable = Disposable.Empty;
+                        }
+                    }
+
+                });
+        }
+
+
+
+
+    }
+}
---
+++ ReactiveUIBug/STAWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using ReactiveUI;
+using Weingartner.ReactiveCompositeCollections;
+
+namespace ReactiveUIBug
+{
+
+    public class CollapsibleLogEntry : LogEntry
+    {
+        public List<LogEntry> Contents { get; set; }
+
+        public CollapsibleLogEntry(DateTime dateTime, string message, int index) : base( dateTime, message, index )
+        {
+        }
+    }
+
+    public class LogEntry 
+    {
+         public DateTime DateTime {get; }
+         public int Index {get; }
+         public string Message {get; }
+
+        public LogEntry(DateTime dateTime, string message, int index)
+        {
+            DateTime = dateTime;
+            Message = message;
+            Index = index;
+        }
+    }
+    /// <summary>
+    /// A simple class for creating a log viewer. Call the static Log message.
+    /// </summary>
+    public partial class LogViewer : Window
+    {
+        private CompositeSourceList<LogEntry> LogEntries { get; set; }
+
+        private LogViewer()
+        {
+            InitializeComponent();
+            LogEntries = new CompositeSourceList<LogEntry>();
+
+            this.LoadUnloadHandler(() => Init());
+        }
+
+        private IEnumerable<IDisposable> Init()
+        {
+            yield return ClearButton
+                .Events()
+                .PreviewMouseUp
+                .Subscribe(_ => ClearAll());
+
+            yield return this
+                .WhenAnyValue(p => p.FilterText.Text)
+                .Subscribe(Console.WriteLine);
+
+            var filteredEntries =
+                LogEntries
+                    //.Where(this
+                    //    .WhenAnyValue(p => p.FilterText.Text)
+                    //    .ObserveOn(this)
+                    //    .Select(str => fun((LogEntry v) => v.Message.Contains(str)))
+                    //)
+                    .CreateObservableCollection(EqualityComparer<LogEntry>.Default);
+            yield return filteredEntries;
+
+            MainPanel.DataContext = filteredEntries;
+            yield return Disposable.Create(() => MainPanel.DataContext = null);
+        }
+
+        public static readonly Lazy<LogViewer> Window = new Lazy<LogViewer>(() => CreateAndShowStaWindow(() => new LogViewer()).Result);
+
+
+        private void ClearAll()
+        {
+            LogEntries.RemoveRange(LogEntries.Source);
+        }
+
+        public static void Invoke(Action a)
+        {
+            Window.Value.Dispatcher.Invoke(a);
+        }
+
+        public static Task<T> CreateAndShowStaWindow<T>(Func<T> factory) where T : Window
+        {
+
+            var windowResult = new TaskCompletionSource<T>();
+
+            var newWindowThread = new Thread(() =>
+            {
+                var window = factory();
+                window.Show();
+                windowResult.SetResult(window);
+                window.Events().Closed.Subscribe(_ => window.Dispatcher.InvokeShutdown());
+                System.Windows.Threading.Dispatcher.Run();
+
+            });
+            newWindowThread.SetApartmentState(ApartmentState.STA);
+            newWindowThread.IsBackground = true;
+            newWindowThread.Start();
+            newWindowThread.Name = "STA WPF";
+
+            return windowResult.Task;
+        }
+
+        private static int _CurrentIndex;
+
+        public static void Log(LogEntry entry)
+        {
+            var window = Window.Value;
+            window.Dispatcher.Invoke(() => window.LogEntries.Add(entry));
+        }
+
+        public static void LogWithIndent(int i, string message)
+        {
+            Log(new string(' ', i * 4) + message);
+
+
+        }
+        public static void Log(string message)
+        {
+            try
+            {
+                Log(new LogEntry(DateTime.Now, message, _CurrentIndex++));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+    }
+
+    public static class LogViewerExtensions
+    {
+
+    }
+}
